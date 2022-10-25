8e18dce9e565c9b1ca504884855a6131c53413af
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-25 14:11:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="Vinyl, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ATN Shop</title>
+    <title>ATN Store</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="shortcut icon" href="ATNtoy\Logo.png">
+    <link rel="shortcut icon" href="logo_ATN.png">
 </head>
 
 <body>
@@ -37,7 +37,7 @@ include_once("connection.php");
     <div class="humberger__menu__overlay"></div>
     <div class="humberger__menu__wrapper">
         <div class="humberger__menu__logo">
-            <a href="?page=content"><img src="ATNtoy/Logo.png" alt=""></a>
+            <a href="?page=content"><img src="logo_ATN.png" alt=""></a>
         </div>
         <div class="humberger__menu__cart">
             <ul>
