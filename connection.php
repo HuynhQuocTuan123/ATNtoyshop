6487b4fc4660ce5c2de6f0e83873fde227942c3d
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-25 14:00:12 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-$conn = pg_connect("postgres://certllyyvshnbk:a93c612f10d1a0d05631ca25c3ded3926e2433125525e99c1e5ab0711a581ad2@ec2-3-213-66-35.compute-1.amazonaws.com:5432/dbdrou5rbubp96
-");
+$conn = pg_connect("postgres://certllyyvshnbk:a93c612f10d1a0d05631ca25c3ded3926e2433125525e99c1e5ab0711a581ad2@ec2-3-213-66-35.compute-1.amazonaws.com:5432/dbdrou5rbubp96");
 
 	if(!$conn)
 	{
