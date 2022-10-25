77ba3f25c0c9d3468dc83282dcec2c2ce9f564a7
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-25 13:58:25 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
-$conn = pg_connect("postgres://jezjickuhvcxio:d1cee8f27ce93b06b3be4560d5ec51eed9545886d0f9ad02f66e613834b84b70@ec2-23-20-140-229.compute-1.amazonaws.com:5432/d2ujo900qie0a5");
+$conn = pg_connect("postgres://certllyyvshnbk:a93c612f10d1a0d05631ca25c3ded3926e2433125525e99c1e5ab0711a581ad2@ec2-3-213-66-35.compute-1.amazonaws.com:5432/dbdrou5rbubp96
+");
 
 	if(!$conn)
 	{
