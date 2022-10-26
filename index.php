4b2939625c99bed86af65d52101166644256ed9e
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-26 11:53:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -405,7 +405,7 @@ include_once("connection.php");
     
 
     Footer Section Begin -->
-    <!--  <footer class="footer spad"> -->
+     <footer class="footer spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
